b5353e70601bab870b2d3a7ca16dd8a5cb4a3140
Johar M. Ashfaque, Ph.D., MInstP, AATQB <39216339+UKVeteran@users.noreply.github.com>
2022-10-02 08:34:24 +0100

    Add files via upload

---
+++ Sumof1-100.kt
@@ -0,0 +1,11 @@
+fun main(args: Array<String>) {
+
+    var sum = 0
+    var i = 100
+
+    while (i != 0) {
+        sum += i     // sum = sum + i;
+        --i
+    }
+    println("sum = $sum")
+}

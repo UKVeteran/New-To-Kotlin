1bab5a1c0c6d24a48676480a7aa5c476d9e2f7c6
Johar M. Ashfaque, Ph.D., MInstP, AATQB <39216339+UKVeteran@users.noreply.github.com>
2022-10-04 06:33:57 +0100

    Add files via upload

---
+++ ComparingStrings/ComparingStrings3.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+
+    val style = buildString { "Bold" }
+    val style2 = buildString { "Bold" }
+
+    if (style === style2)
+        println("Equal")
+    else
+        println("Not Equal")
+}

48960e2aa9508dd11960164bcf0c591008fe4acf
Johar M. Ashfaque, Ph.D., MInstP, AATQB <39216339+UKVeteran@users.noreply.github.com>
2022-10-06 20:54:45 +0100

    Add files via upload

---
+++ Pyramid Patterns/E3.kt
@@ -0,0 +1,13 @@
+fun main(args: Array<String>) {
+    val last = 'E'
+    var alphabet = 'A'
+
+    for (i in 1..last - 'A' + 1) {
+        for (j in 1..i) {
+            print("$alphabet ")
+        }
+        ++alphabet
+
+        println()
+    }
+}

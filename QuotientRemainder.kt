00f5584552980363effd67cc8aeb22a6c5ecf157
Johar M. Ashfaque, Ph.D., MInstP, AATQB <39216339+UKVeteran@users.noreply.github.com>
2022-10-11 22:22:19 +0100

    Add files via upload

---
+++ QuotientRemainder.kt
@@ -0,0 +1,11 @@
+fun main(args: Array<String>) {
+
+    val dividend = 20
+    val divisor = 3
+
+    val quotient = dividend / divisor
+    val remainder = dividend % divisor
+
+    println("Quotient = $quotient")
+    println("Remainder = $remainder")
+}

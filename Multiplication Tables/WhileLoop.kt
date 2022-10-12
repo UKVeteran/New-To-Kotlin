3a00121c5e70de845038d5ae9401c0e2f2e62769
Johar M. Ashfaque, Ph.D., MInstP, AATQB <39216339+UKVeteran@users.noreply.github.com>
2022-10-12 11:31:43 +0100

    Add files via upload

---
+++ Multiplication Tables/WhileLoop.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+    val num = 20
+    var i = 1
+    
+    while (i <= 10) {
+        val product = num * i
+        println("$num * $i = $product")
+        i++
+    }
+}
